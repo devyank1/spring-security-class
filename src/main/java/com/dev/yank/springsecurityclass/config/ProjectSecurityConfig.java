525649bb2e97f9b716ffb6d488d830b7154f9d15
Yan Carlos <contact@yancarlos.tech>
2025-04-02 19:08:37 -0300

    add the compromised password checker to analyze the pwd and force the user to uses a strong pwd.

--- src/main/java/com/dev/yank/springsecurityclass/config/ProjectSecurityConfig.java
+++ src/main/java/com/dev/yank/springsecurityclass/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.dev.yank.springsecurityclass.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -30,12 +32,17 @@ public class ProjectSecurityConfig {
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
         UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$tIF5r3Yyh7.UeEucJ0e3Rej7tT7orN6tSFzO1a0KwPss4wF6f9GOK").authorities("admin").build();
+                .password("{bcrypt}$2a$12$RMLvlcJrha.vcx5wl//8Eus3K/ZxelrcYyC6wlKQkJK9Qv.lSSDUe").authorities("admin").build();
         return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Bean
     public PasswordEncoder encoder() { // this method is to create a password encoder
         return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // factories it's better than encrypt because they pick any password hash.
+    }
+
+    @Bean
+    public CompromisedPasswordChecker checker() { // this helps to check if pwd is compromised in any data breaches.
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
