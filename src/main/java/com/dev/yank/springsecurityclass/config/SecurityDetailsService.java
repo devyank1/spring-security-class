f1cb9ddf2e2e14ab2ddfcd56190ee0be764d22fa
Yan Carlos <contact@yancarlos.tech>
2025-04-22 14:33:29 -0300

    added load by username, find email in DB, fetch the username and compare the passwords to login

--- src/main/java/com/dev/yank/springsecurityclass/config/SecurityDetailsService.java
+++ src/main/java/com/dev/yank/springsecurityclass/config/SecurityDetailsService.java
@@ -19,13 +19,11 @@ public class SecurityDetailsService implements UserDetailsService {
         this.customerRepository = customerRepository;
     }
 
-    @Override //
+    @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User details not found for the user: " + username));
+        Customer customer = customerRepository.findByEmail(username).orElseThrow(() -> new
+                UsernameNotFoundException("User details not found for the user: " + username));
         List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
-
         return new User(customer.getEmail(), customer.getPassword(), authorities);
     }
-
-
 }
